d307fb87153932d7ccfbedaa44b2a818b129188b
renan filipe <renanfilipe10@hotmail.com>
2022-09-27 20:01:13 -0300

    change url to https

--- src/api/fetchCityNames.js
+++ src/api/fetchCityNames.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 function fetchCityNames(value) {
   return axios.get(
-    "http://dataservice.accuweather.com/locations/v1/cities/autocomplete",
+    "https://dataservice.accuweather.com/locations/v1/cities/autocomplete",
     {
       params: { apikey: process.env.REACT_APP_ACCU_WEATHER_API_KEY, q: value },
     }
